22f5408d5cc925c6b66753fa40212e73f22143fc
medegor44 <medegor44@gmail.com>
2024-06-09 12:10:12 +0300

    Initial commit

---
+++ WebApi/Controllers/NoteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi;
+
+public class NoteDto
+{
+    public string Header { get; set; }
+    public string Body { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public List<string> Tags { get; set; }
+    public string Author { get; set; }
+    public Guid? Id { get; set; }
+}
---
+++ WebApi/Controllers/NotesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi;
+
+[ApiController]
+[Route("[controller]")]
+public class NotesController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(Guid))]
+    public Task<IResult> CreateNote([FromBody] NoteDto note)
+    {
+        return Task.FromResult(Results.Ok(Guid.NewGuid()));
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(NoteDto))]
+    public Task<IResult> GetNote([FromRoute] Guid id)
+    {
+        return Task.FromResult(Results.Ok(new NoteDto()));
+    }
+}
